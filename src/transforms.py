e170a62ed5b700f9af141610c69e2282d6ff9dbe
iambackend <35529987+iambackend@users.noreply.github.com>
2021-11-25 23:26:00 +0300

    fix resize and normalization

--- src/transforms.py
+++ src/transforms.py
@@ -2,7 +2,9 @@ from torchvision import transforms
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
-     transforms.Resize((100, 100)), # Hint: this might not be the best way to resize images
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # Hint: this might not be the best normalization
+     transforms.Resize((224, 224)), # Hint: this might not be the best way to resize images
+     transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]) # Hint: this might not be the best normalization
      ]
 )
