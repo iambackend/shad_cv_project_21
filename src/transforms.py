d036853ffecf7d427ef30342e7f82d01ef4e5f32
iambackend <35529987+iambackend@users.noreply.github.com>
2021-11-29 09:50:40 +0300

    add augmentations

--- src/transforms.py
+++ src/transforms.py
@@ -1,10 +1,23 @@
 from torchvision import transforms
+import torch
+
+train_transform = transforms.Compose(
+    [transforms.ToTensor(), 
+     transforms.Resize((224, 224)), # Hint: this might not be the best way to resize images
+     transforms.ConvertImageDtype(torch.uint8),
+     transforms.AutoAugment(transforms.autoaugment.AutoAugmentPolicy.IMAGENET),
+     transforms.ConvertImageDtype(torch.float32),
+     transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]), # Hint: this might not be the best normalization
+     ]
+)
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Resize((224, 224)), # Hint: this might not be the best way to resize images
      transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]) # Hint: this might not be the best normalization
-     ]
+        std=[0.229, 0.224, 0.225]), # Hint: this might not be the best normalization
+    ]
 )
